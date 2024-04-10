27960a5dfef48f9272d48126db99161698ee834d
mrgsdev <mrgsdev@gmail.com>
2024-04-10 14:56:35 +0300

    Add FoodPin App

---
+++ UIKit/Beginning iOS Programming with Swift/FoodPin/FoodPin/FoodPin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
