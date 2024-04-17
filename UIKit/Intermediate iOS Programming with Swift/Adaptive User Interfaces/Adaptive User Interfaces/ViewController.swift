61c2001918b3631ba01f18a0638241e02b59206c
mrgsdev <mrgsdev@gmail.com>
2024-04-17 10:45:33 +0300

    Chapter 1 Building Adaptive User Interfaces

---
+++ UIKit/Intermediate iOS Programming with Swift/Adaptive User Interfaces/Adaptive User Interfaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adaptive User Interfaces
+//
+//  Created by mrgsdev on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
