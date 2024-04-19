c07fd5df45d9c1661e4b32897ecac9eed8d927be
mrgsdev <mrgsdev@gmail.com>
2024-04-19 15:00:22 +0300

    Chapter 2 Creating Table View Sections and Index list with Diffable Data Source

---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 2/Chapter 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter 2
+//
+//  Created by mrgsdev on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
