85f6ae77f86ef9adfe6ab2c396c570f8d1e65ca5
mrgsdev <mrgsdev@gmail.com>
2024-04-22 10:44:04 +0300

    Chapter 4 Working with JSON and Codable in Swift

--- UIKit/Intermediate iOS Programming with Swift/Chapter 4/Chapter 4/KivaLoanTableViewController.swift
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 4/Chapter 4/KivaLoanTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
-    
+
     enum Section {
         case all
     }
@@ -20,48 +20,50 @@ class KivaLoanTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.estimatedRowHeight = 92.0
         tableView.rowHeight = UITableView.automaticDimension
         
         getLatestLoans()
     }
-}
 
-extension KivaLoanTableViewController {
+   
+    
+}
+extension KivaLoanTableViewController{
     func configureDataSource() -> UITableViewDiffableDataSource<Section, Loan> {
-        
+
         let cellIdentifier = "Cell"
-        
+
         let dataSource = UITableViewDiffableDataSource<Section, Loan>(
             tableView: tableView,
             cellProvider: {  tableView, indexPath, loan in
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! KivaLoanTableViewCell
-                
+
                 cell.nameLabel.text = loan.name
                 cell.countryLabel.text = loan.country
                 cell.useLabel.text = loan.use
                 cell.amountLabel.text = "$\(loan.amount)"
-                
+
                 return cell
             }
         )
-        
+
         return dataSource
     }
-    
+
     func updateSnapshot(animatingChange: Bool = false) {
-        
+
         // Create a snapshot and populate the data
         var snapshot = NSDiffableDataSourceSnapshot<Section, Loan>()
         snapshot.appendSections([.all])
         snapshot.appendItems(loans, toSection: .all)
-        
+
         dataSource.apply(snapshot, animatingDifferences: animatingChange)
     }
 }
 
-extension KivaLoanTableViewController {
+extension KivaLoanTableViewController  {
     func getLatestLoans() {
         guard let loanUrl = URL(string: kivaLoanURL) else {
             return
@@ -70,13 +72,13 @@ extension KivaLoanTableViewController {
         let request = URLRequest(url: loanUrl)
         let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
             
-            if let error {
+            if let error = error {
                 print(error)
                 return
             }
             
             // Parse JSON data
-            if let data {
+            if let data = data {
                 self.loans = self.parseJsonData(data: data)
                 
                 // Update table view's data
@@ -88,25 +90,16 @@ extension KivaLoanTableViewController {
         
         task.resume()
     }
-    
+
     func parseJsonData(data: Data) -> [Loan] {
         
         var loans = [Loan]()
         
+        let decoder = JSONDecoder()
+        
         do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-            
-            // Parse JSON data
-            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
-            for jsonLoan in jsonLoans {
-                var loan = Loan()
-                loan.name = jsonLoan["name"] as! String
-                loan.amount = jsonLoan["loan_amount"] as! Int
-                loan.use = jsonLoan["use"] as! String
-                let location = jsonLoan["location"] as! [String:AnyObject]
-                loan.country = location["country"] as! String
-                loans.append(loan)
-            }
+            let loanDataStore = try decoder.decode(LoanDataStore.self, from: data)
+            loans = loanDataStore.loans
             
         } catch {
             print(error)
@@ -114,4 +107,5 @@ extension KivaLoanTableViewController {
         
         return loans
     }
+    
 }
--- UIKit/Intermediate iOS Programming with Swift/Chapter 4/Chapter 4/Loan.swift
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 4/Chapter 4/Loan.swift
@@ -7,11 +7,41 @@
 
 import Foundation
  
-struct Loan: Hashable {
+struct Loan: Hashable, Codable {
     
     var name: String = ""
     var country: String = ""
     var use: String = ""
     var amount: Int = 0
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case country = "location"
+        case use
+        case amount = "loan_amount"
+    }
     
+    enum LocationKeys: String, CodingKey {
+        case country
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try values.decode(String.self, forKey: .name)
+        
+        let location = try values.nestedContainer(keyedBy: LocationKeys.self, forKey: .country)
+        country = try location.decode(String.self, forKey: .country)
+        
+        use = try values.decode(String.self, forKey: .use)
+        amount = try values.decode(Int.self, forKey: .amount)
+        
+    }
 }
+
+
+struct LoanDataStore: Codable {
+    var loans: [Loan]
+}
+ 
+ 
