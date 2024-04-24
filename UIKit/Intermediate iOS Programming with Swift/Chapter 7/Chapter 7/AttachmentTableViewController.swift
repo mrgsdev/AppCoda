40216d38138e54753c0c86b5b48f65a50d377ff2
mrgsdev <mrgsdev@gmail.com>
2024-04-24 10:13:53 +0300

    Chapter 7 Sending SMS and MMS Using MessageUI Framework

--- UIKit/Intermediate iOS Programming with Swift/Chapter 7/Chapter 7/AttachmentTableViewController.swift
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 7/Chapter 7/AttachmentTableViewController.swift
@@ -9,125 +9,96 @@ import UIKit
 import MessageUI
 class AttachmentTableViewController: UITableViewController {
     
-    let filenames = [
-        "10 Great iPhone Tips.pdf", "camera-photo-tips.html", "foggy.jpg",
-        "Hello World.ppt", "no more complaint.png", "Why Appcoda.doc"
-    ]
-    
-    enum MIMEType: String {
-        case jpg = "image/jpeg"
-        case png = "image/png"
-        case doc = "application/msword"
-        case ppt = "application/vnd.ms-powerpoint"
-        case html = "text/html"
-        case pdf = "application/pdf"
-        
-        init?(type: String) {
-            switch type.lowercased() {
-            case "jpg": self = .jpg
-            case "png": self = .png
-            case "doc": self = .doc
-            case "ppt": self = .ppt
-            case "html": self = .html
-            case "pdf": self = .pdf
-            default: return nil
-            }
-        }
-    }
+    let filenames = ["10 Great iPhone Tips.pdf", "camera-photo-tips.html", "foggy.jpg", "Hello World.ppt", "no more complaint.png", "Why Appcoda.doc"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.prefersLargeTitles = true
         title = "Simple Attachment"
+        // Enable large title
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         // Return the number of sections
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows
         return filenames.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-         
+        
+        // Configure the cell...
         cell.textLabel?.text = filenames[indexPath.row]
         cell.imageView?.image = UIImage(named: "icon\(indexPath.row)");
         
         return cell
     }
     
-    // MARK: - UITableViewDelegate
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedFile = filenames[indexPath.row]
-        tableView.deselectRow(at: indexPath, animated: true)
-        showEmail(attachment: selectedFile)
+        sendSMS(attachment: selectedFile)
     }
     
-    // MARK: - Helper
+    // MARK: - Private methods
     
-    func showEmail(attachment: String) {
+    private func sendSMS(attachment: String) {
         
-        // Check if the device is capable to send email
-        guard MFMailComposeViewController.canSendMail() else {
-            print("This device doesn't allow you to send mail.")
+        // Check if the device is capable of sending text message
+        guard MFMessageComposeViewController.canSendText() else {
+            let alertMessage = UIAlertController(title: "SMS Unavailable", message: "Your device is not capable of sending SMS.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertMessage, animated: true, completion: nil)
             return
         }
         
-        let emailTitle = "Great Photo and Doc"
-        let messageBody = "Hey, check this out!"
-        let toRecipients = ["mrgsdev@gmail.com"]
+        // Prefill the SMS
+        let messageController = MFMessageComposeViewController()
+        messageController.messageComposeDelegate = self
+        messageController.recipients = ["12345678", "72345524"]
+        messageController.body = "Just sent the \(attachment) to your email. Please check!"
         
-        // Initialize the mail composer and populate the mail content
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.mailComposeDelegate = self
-        mailComposer.setSubject(emailTitle)
-        mailComposer.setMessageBody(messageBody, isHTML: false)
-        mailComposer.setToRecipients(toRecipients)
-        
-        // Determine the file name and extension
+        // Adding file attachment
         let fileparts = attachment.components(separatedBy: ".")
         let filename = fileparts[0]
         let fileExtension = fileparts[1]
+        let filePath = Bundle.main.path(forResource: filename, ofType: fileExtension)
+        let fileUrl = NSURL.fileURL(withPath: filePath!)
+        messageController.addAttachmentURL(fileUrl, withAlternateFilename: nil)
         
-        // Get the resource path and read the file using NSData
-        guard let filePath = Bundle.main.path(forResource: filename, ofType: fileExtension) else {
-            return
-        }
-        
-        // Get the file data and MIME type
-        if let fileData = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
-           let mimeType = MIMEType(type: fileExtension) {
-            
-            // Add attachment
-            mailComposer.addAttachmentData(fileData, mimeType: mimeType.rawValue, fileName: filename)
-            
-            // Present mail view controller on screen
-            present(mailComposer, animated: true, completion: nil)
-        }
+        // Present message view controller on screen
+        present(messageController, animated: true, completion: nil)
     }
+
 }
 
-extension AttachmentTableViewController: MFMailComposeViewControllerDelegate {
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+extension AttachmentTableViewController: MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
         
-        switch result {
-        case MFMailComposeResult.cancelled:
-            print("Mail cancelled")
-        case MFMailComposeResult.saved:
-            print("Mail saved")
-        case MFMailComposeResult.sent:
-            print("Mail sent")
-        case MFMailComposeResult.failed:
-            print("Failed to send: \(error?.localizedDescription ?? "")")
+        switch(result) {
+        case MessageComposeResult.cancelled:
+            print("SMS cancelled")
+            
+        case MessageComposeResult.failed:
+            let alertMessage = UIAlertController(title: "Failure", message: "Failed to send the message.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertMessage, animated: true, completion: nil)
+            
+        case MessageComposeResult.sent:
+            print("SMS sent")
+            
         @unknown default:
-            print("Unknown error: \(error.debugDescription)")
+            print("Unknown error")
         }
         
         dismiss(animated: true, completion: nil)
