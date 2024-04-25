a3f7770144e27d5623ba2fc3bf4837c3f1fefae0
mrgsdev <mrgsdev@gmail.com>
2024-04-25 10:41:00 +0300

    Chapter 8 How to Get Direction and Draw Route on Maps

---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 8/Chapter 8/MapViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MapViewController.swift
+//  Chapter 8
+//
+//  Created by mrgsdev on 25.04.2024.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var restaurant:Restaurant!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+            
+        })
+        
+        mapView.delegate = self
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - MKMapViewDelegate methods
+
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKAnnotationView?
+        
+        if #available(iOS 11.0, *) {
+            var markerAnnotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+            
+            if markerAnnotationView == nil {
+                markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                markerAnnotationView?.canShowCallout = true
+            }
+            
+            markerAnnotationView?.glyphText = "😋"
+            markerAnnotationView?.markerTintColor = UIColor.orange
+        
+            annotationView = markerAnnotationView
+            
+        } else {
+            
+            var pinAnnotationView: MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+            
+            if pinAnnotationView == nil {
+                pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                pinAnnotationView?.canShowCallout = true
+                pinAnnotationView?.pinTintColor = UIColor.orange
+            }
+            
+            annotationView = pinAnnotationView
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named: restaurant.image)
+        annotationView?.leftCalloutAccessoryView = leftIconView
+            
+        return annotationView
+    }
+}
---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 8/Chapter 8/Restaurant.swift
@@ -0,0 +1,26 @@
+//
+//  Restaurant.swift
+//  Chapter 8
+//
+//  Created by mrgsdev on 25.04.2024.
+//
+
+import Foundation
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    var phone = ""
+    var rating = ""
+    
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
