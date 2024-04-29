ab1b3e5080c03cf8cc677448da1c16a0ae86124d
mrgsdev <mrgsdev@gmail.com>
2024-04-29 10:37:58 +0300

    Chapter 12 Working with URL Schemes

---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 12/Chapter 12/QRScannerController.swift
@@ -0,0 +1,139 @@
+//
+//  QRScannerController.swift
+//  Chapter 12
+//
+//  Created by mrgsdev on 29.04.2024.
+//
+
+import UIKit
+import AVFoundation
+
+class QRScannerController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var topbar: UILabel!
+    var captureSession = AVCaptureSession()
+var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+var qrCodeFrameView: UIView?
+
+private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+                                  AVMetadataObject.ObjectType.code39,
+                                  AVMetadataObject.ObjectType.code39Mod43,
+                                  AVMetadataObject.ObjectType.code93,
+                                  AVMetadataObject.ObjectType.code128,
+                                  AVMetadataObject.ObjectType.ean8,
+                                  AVMetadataObject.ObjectType.ean13,
+                                  AVMetadataObject.ObjectType.aztec,
+                                  AVMetadataObject.ObjectType.pdf417,
+                                  AVMetadataObject.ObjectType.itf14,
+                                  AVMetadataObject.ObjectType.dataMatrix,
+                                  AVMetadataObject.ObjectType.interleaved2of5,
+                                  AVMetadataObject.ObjectType.qr]
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Get the back-facing camera for capturing videos
+    guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+        print("Failed to get the camera device")
+        return
+    }
+
+    do {
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+        let input = try AVCaptureDeviceInput(device: captureDevice)
+        
+        // Set the input device on the capture session.
+        captureSession.addInput(input)
+        
+        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession.addOutput(captureMetadataOutput)
+        
+        // Set delegate and use the default dispatch queue to execute the call back
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Start video capture.
+        DispatchQueue.global(qos: .background).async {
+            self.captureSession.startRunning()
+        }
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubviewToFront(qrCodeFrameView)
+        }
+        
+    } catch {
+        // If any error occurs, simply print it out and don't continue any more.
+        print(error)
+        return
+    }
+    
+    // Move the message label and top bar to the front
+    view.bringSubviewToFront(messageLabel)
+    view.bringSubviewToFront(topbar)
+}
+
+func launchApp(decodedURL: String) {
+    if presentedViewController != nil {
+        return
+    }
+    
+    let alertPrompt = UIAlertController(title: "Open App", message: "You're going to open \(decodedURL)", preferredStyle: .actionSheet)
+    let confirmAction = UIAlertAction(title: "Confirm", style: .default, handler: { (action) -> Void in
+        
+        if let url = URL(string: decodedURL) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    })
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    
+    alertPrompt.addAction(confirmAction)
+    alertPrompt.addAction(cancelAction)
+    
+    present(alertPrompt, animated: true, completion: nil)
+}
+}
+
+extension QRScannerController: AVCaptureMetadataOutputObjectsDelegate {
+
+func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    // Check if the metadataObjects array is not nil and it contains at least one object.
+    if metadataObjects.count == 0 {
+        qrCodeFrameView?.frame = CGRect.zero
+        messageLabel.text = "No QR code is detected"
+        return
+    }
+    
+    // Get the metadata object.
+    let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+    
+    
+    if supportedCodeTypes.contains(metadataObj.type) {
+        // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+        let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+        qrCodeFrameView?.frame = barCodeObject!.bounds
+        
+        if metadataObj.stringValue != nil {
+            messageLabel.text = metadataObj.stringValue
+            launchApp(decodedURL: metadataObj.stringValue!)
+        }
+    }
+
+}
+}
---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 12/TextReader/TextReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextReader
+//
+//  Created by mrgsdev on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
