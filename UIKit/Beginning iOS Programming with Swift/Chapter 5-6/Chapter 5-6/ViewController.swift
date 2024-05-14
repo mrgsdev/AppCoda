bdb55c09d4ab786215ffb0b41d53f46cf2ea1ee1
mrgsdev <mrgsdev@gmail.com>
2024-05-14 07:53:49 +0300

    Chapter 5-6

---
+++ UIKit/Beginning iOS Programming with Swift/Chapter 5-6/Chapter 5-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter 5-6
+//
+//  Created by mrgsdev on 09.04.2024
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
