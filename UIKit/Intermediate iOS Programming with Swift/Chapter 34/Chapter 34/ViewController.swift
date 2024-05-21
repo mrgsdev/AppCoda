241cdf89d195d8a080eff80c272fa4aa50b2166c
mrgsdev <mrgsdev@gmail.com>
2024-05-21 08:11:24 +0300

    Chapter 34 Building a Simple Sticker App

---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 34/Chapter 34/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter 34
+//
+//  Created by mrgsdev on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
