f34aaf88f240ed8db1d8f96fe6b7b030070325b7
mrgsdev <mrgsdev@gmail.com>
2024-05-23 14:01:59 +0300

    Chapter 36 Building Custom UI Components Using IBDesignable and IBInspectable

---
+++ UIKit/Intermediate iOS Programming with Swift/Chapter 36/Chapter 36/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter 36
+//
+//  Created by mrgsdev on 23.05.2024
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
