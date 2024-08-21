f8fe9517d664ee8d98b7486cc68d4c01de5ea2cd
mrgsdev <mrgsdev@gmail.com>
2024-08-21 12:25:10 +0300

    Chapter 6: Exercise

---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/Exercises/Chapter 6/Chapter 6/Chapter 6/Chapter_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter_6App.swift
+//  Chapter 6
+//
+//  Created by mrgsdev on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chapter_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
