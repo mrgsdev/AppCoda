3cbe573505757d7ae4bb8e2d7ed9bebf0beda89d
mrgsdev <mrgsdev@gmail.com>
2024-08-25 18:04:25 +0300

    Chapter 12: Detail View Enhancement, Custom Fonts and Navigation Bar Customization

---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/FoodPin/FoodPin/View/RestaurantDetailView.swift
@@ -0,0 +1,89 @@
+//
+//  RestaurantDetailView.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 25.08.2024.
+//
+
+import SwiftUI
+
+struct RestaurantDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var restaurant: Restaurant
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Image(restaurant.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 445)
+                    .overlay {
+                        VStack {
+                            Image(systemName: restaurant.isFavorite ? "heart.fill" : "heart")
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+                                .padding()
+                                .font(.system(size: 30))
+                                .foregroundStyle(restaurant.isFavorite ? .yellow : .white)
+                                .padding(.top, 40)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(restaurant.name)
+                                    .font(.custom("Nunito-Regular", size: 35, relativeTo: .largeTitle))
+                                    .bold()
+                                Text(restaurant.type)
+                                    .font(.system(.headline, design: .rounded))
+                                    .padding(.all, 5)
+                                    .background(.black)
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+                            .foregroundStyle(.white)
+                            .padding()
+                        }
+                    }
+                
+                Text(restaurant.description)
+                    .padding()
+                
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        Text("ADDRESS")
+                            .font(.system(.headline, design: .rounded))
+                        
+                        Text(restaurant.location)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    
+                    VStack(alignment: .leading) {
+                        Text("PHONE")
+                            .font(.system(.headline, design: .rounded))
+                        
+                        Text(restaurant.phone)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Text("\(Image(systemName: "chevron.left"))")
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    NavigationStack {
+        RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
+    }
+    .tint(.white)
+}
