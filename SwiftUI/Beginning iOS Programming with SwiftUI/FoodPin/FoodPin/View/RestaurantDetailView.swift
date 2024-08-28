9deea97c3b729ef6f95603eb501de978a6ba4b3e
mrgsdev <mrgsdev@gmail.com>
2024-08-28 15:06:59 +0300

    Chapter 14

--- SwiftUI/Beginning iOS Programming with SwiftUI/FoodPin/FoodPin/View/RestaurantDetailView.swift
+++ SwiftUI/Beginning iOS Programming with SwiftUI/FoodPin/FoodPin/View/RestaurantDetailView.swift
@@ -65,6 +65,20 @@ struct RestaurantDetailView: View {
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                 }
                 .padding(.horizontal)
+                
+                NavigationLink(
+                    destination:
+                        MapView(location: restaurant.location)
+                            .toolbarBackground(.hidden, for: .navigationBar)
+                            .edgesIgnoringSafeArea(.all)
+                            
+                ) {
+                    MapView(location: restaurant.location, interactionMode: [])
+                        .frame(height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .padding()
+                }
+                 
             }
         }
         .navigationBarBackButtonHidden(true)
@@ -78,13 +92,15 @@ struct RestaurantDetailView: View {
             }
         }
         .ignoresSafeArea()
+        .toolbarBackground(.hidden, for: .navigationBar)
     }
 }
 
 #Preview {
     NavigationStack {
         RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
-            .environment(\.dynamicTypeSize, .xxxLarge)
+        
+            .toolbarBackground(.hidden, for: .navigationBar)
     }
     .tint(.white)
 }
