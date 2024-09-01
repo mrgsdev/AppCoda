55a92904952f6df84c377f5a029709d5c68050c1
mrgsdev <mrgsdev@gmail.com>
2024-09-01 16:22:29 +0300

    Chapter 18: Exercise

---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/Exercises/Chapter 18/FoodPin/View/ActivityView.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityView.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 24.08.2024.
+//
+
+import SwiftUI
+
+struct ActivityView: UIViewControllerRepresentable {
+    
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return activityController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/Exercises/Chapter 18/FoodPin/View/RestaurantListView.swift
@@ -0,0 +1,204 @@
+//
+//  RestaurantListView.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 21.08.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct RestaurantListView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query var restaurants: [Restaurant]
+    @State private var showNewRestaurant = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                if restaurants.count == 0 {
+                    Image("emptydata")
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ForEach(restaurants.indices, id: \.self) { index in
+                        ZStack(alignment: .leading) {
+                            NavigationLink(destination: RestaurantDetailView(restaurant: restaurants[index])) {
+                                EmptyView()
+                            }
+                            .opacity(0)
+                            
+                            BasicTextImageRow(restaurant: restaurants[index])
+                        }
+                    }
+                    .onDelete(perform: deleteRecord)
+                    .listRowSeparator(.hidden)
+                }
+            }
+            .listStyle(.plain)
+            
+            .navigationTitle("FoodPin")
+            .navigationBarTitleDisplayMode(.automatic)
+            
+            .toolbar {
+                Button(action: {
+                    self.showNewRestaurant = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .tint(.primary)
+        .sheet(isPresented: $showNewRestaurant) {
+            NewRestaurantView()
+        }
+    }
+    
+    private func deleteRecord(indexSet: IndexSet) {
+        
+          for index in indexSet {
+              let itemToDelete = restaurants[index]
+              modelContext.delete(itemToDelete)
+          }
+    }
+}
+
+#Preview {
+    RestaurantListView()
+}
+
+#Preview("Dark mode") {
+    RestaurantListView()
+        .preferredColorScheme(.dark)
+}
+
+#Preview("BasicTextImageRow", traits: .sizeThatFitsLayout) {
+    BasicTextImageRow(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: UIImage(named: "cafedeadend")!, isFavorite: true))
+}
+
+#Preview("FullImageRow", traits: .sizeThatFitsLayout) {
+    FullImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+}
+
+struct BasicTextImageRow: View {
+    // MARK: - Binding
+    
+    @Bindable var restaurant: Restaurant
+    
+    // MARK: - State variables
+    
+    @State private var showOptions = false
+    @State private var showError = false
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 20) {
+            Image(uiImage: restaurant.image)
+                .resizable()
+                .frame(width: 120, height: 118)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            VStack(alignment: .leading) {
+                Text(restaurant.name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(restaurant.type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(restaurant.location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundStyle(.gray)
+            }
+            
+            if restaurant.isFavorite {
+                Spacer()
+                
+                Image(systemName: "heart.fill")
+                    .foregroundStyle(.yellow)
+            }
+        }
+        .contextMenu {
+            Button(action: {
+                self.showError.toggle()
+            }) {
+                HStack {
+                    Text("Reserve a table")
+                    Image(systemName: "phone")
+                }
+            }
+            
+            Button(action: {
+                self.restaurant.isFavorite.toggle()
+            }) {
+                HStack {
+                    Text(restaurant.isFavorite ? "Remove from favorites" : "Mark as favorite")
+                    Image(systemName: "heart")
+                }
+            }
+            
+            Button(action: {
+                self.showOptions.toggle()
+            }) {
+                HStack {
+                    Text("Share")
+                    Image(systemName: "square.and.arrow.up")
+                }
+            }
+        }
+        .alert("Not yet available", isPresented: $showError) {
+            Button("OK") {}
+        } message: {
+            Text("Sorry, this feature is not available yet. Please retry later.")
+        }
+        .sheet(isPresented: $showOptions) {
+            
+            let defaultText = "Just checking in at \(restaurant.name)"
+            
+            ActivityView(activityItems: [defaultText, restaurant.image])
+        }
+    }
+}
+
+struct FullImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text(name)
+                        .font(.system(.title2, design: .rounded))
+                        
+                    Text(type)
+                        .font(.system(.body, design: .rounded))
+                    
+                    Text(location)
+                        .font(.system(.subheadline, design: .rounded))
+                        .foregroundStyle(.gray)
+                }
+                
+                if isFavorite {
+                    Spacer()
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundStyle(.yellow)
+                }
+
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+    }
+}
