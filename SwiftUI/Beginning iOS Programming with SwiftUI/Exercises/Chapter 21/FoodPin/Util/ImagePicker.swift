b850b421e09c10f3cf7693ed7cd54979eae904e1
mrgsdev <mrgsdev@gmail.com>
2024-09-04 00:52:29 +0300

    Chapter 21: Exercise

---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/Exercises/Chapter 21/FoodPin/Util/ImagePicker.swift
@@ -0,0 +1,54 @@
+//
+//  ImagePicker.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 31.08.2024.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    @Binding var selectedImage: UIImage
+    @Environment(\.dismiss) private var dismiss
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            
+            parent.dismiss()
+        }
+    }
+}
+
