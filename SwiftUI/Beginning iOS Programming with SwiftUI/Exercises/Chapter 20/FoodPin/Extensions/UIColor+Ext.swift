5dca4265f2e5adf0ff1ef250e7f3a73845311f89
mrgsdev <mrgsdev@gmail.com>
2024-09-03 12:47:54 +0300

    Chapter 20: Exercise

---
+++ SwiftUI/Beginning iOS Programming with SwiftUI/Exercises/Chapter 20/FoodPin/Extensions/UIColor+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor.swift
+//  FoodPin
+//
+//  Created by mrgsdev on 26.08.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let redValue = CGFloat(red) / 255.0
+        let greenValue = CGFloat(green) / 255.0
+        let blueValue = CGFloat(blue) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
+}
