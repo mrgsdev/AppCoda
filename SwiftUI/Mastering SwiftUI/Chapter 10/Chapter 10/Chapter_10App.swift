7563b6ae24ef9a2c35d3f2b3f73c20e70e9c8d6c
mrgsdev <mrgsdev@gmail.com>
2024-09-20 00:02:04 +0300

    Chapter 10

---
+++ SwiftUI/Mastering SwiftUI/Chapter 10/Chapter 10/Chapter_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter_10App.swift
+//  Chapter 10
+//
+//  Created by mrgsdev on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chapter_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/Mastering SwiftUI/Chapter 10/Chapter 10/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  Chapter 10
+//
+//  Created by mrgsdev on 10.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                   Restaurant(name: "Homei", image: "homei"),
+                   Restaurant(name: "Teakha", image: "teakha"),
+                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                   Restaurant(name: "Po's Atelier", image: "posatelier"),
+                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                   Restaurant(name: "Homei", image: "upstate"),
+                   Restaurant(name: "Traif", image: "traif"),
+                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                   Restaurant(name: "Cafe Lore", image: "cafelore"),
+                   Restaurant(name: "Confessional", image: "confessional"),
+                   Restaurant(name: "Barrafina", image: "barrafina"),
+                   Restaurant(name: "Donostia", image: "donostia"),
+                   Restaurant(name: "Royal Oak", image: "royaloak"),
+                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+    ]
+    
+    var body: some View {
+        List(restaurants) { restaurant in
+            ForEach(restaurants.indices, id: \.self) { index in
+                if (0...1).contains(index) {
+                    FullImageRow(restaurant: self.restaurants[index])
+                } else {
+                    BasicImageRow(restaurant: self.restaurants[index])
+                }
+            }
+            
+            .listRowSeparator(.hidden)
+        }
+        .background {
+            Image("homei")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .ignoresSafeArea()
+        }
+        .scrollContentBackground(.hidden)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+}
+
+struct BasicImageRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        HStack {
+            Image(restaurant.image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(restaurant.name)
+        }
+    }
+}
+
+struct FullImageRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        ZStack {
+            Image(restaurant.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .cornerRadius(10)
+                .overlay(
+                    Rectangle()
+                        .foregroundStyle(.black)
+                        .cornerRadius(10)
+                        .opacity(0.2)
+                )
+            
+            Text(restaurant.name)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+        }
+    }
+}
+
