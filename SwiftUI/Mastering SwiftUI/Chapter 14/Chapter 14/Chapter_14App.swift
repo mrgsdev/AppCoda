09b01354825dc355f11b8691286c934a632241af
mrgsdev <mrgsdev@gmail.com>
2024-09-23 16:35:18 +0300

    Chapter 14

---
+++ SwiftUI/Mastering SwiftUI/Chapter 14/Chapter 14/Chapter_14App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter_14App.swift
+//  Chapter 14
+//
+//  Created by mrgsdev on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chapter_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/Mastering SwiftUI/Chapter 14/Chapter 14/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chapter 14
+//
+//  Created by mrgsdev on 23.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
