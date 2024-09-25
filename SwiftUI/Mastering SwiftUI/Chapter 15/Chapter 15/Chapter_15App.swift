64c9df787bdc111b049a2a9f053c2bcbb31f1c7e
mrgsdev <mrgsdev@gmail.com>
2024-09-25 14:20:16 +0300

    Chapter 15

---
+++ SwiftUI/Mastering SwiftUI/Chapter 15/Chapter 15/Chapter_15App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter_15App.swift
+//  Chapter 15
+//
+//  Created by mrgsdev on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chapter_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
